76f7d52dd80d8c10c2ab734377c9801abc854fc5
gwy15 <gwy15thu@gmail.com>
2020-03-08 18:05:22 +0800

    Refactor

--- main.py
+++ main.py
@@ -2,108 +2,163 @@ from pathlib import Path
 import shutil
 import asyncio
 import datetime
+from time import time
 from urllib.parse import quote
+from dataclasses import dataclass
+from typing import List, Generator
 
+import click
 import aiohttp
 import pyppeteer
 from pyppeteer import launch
 
 PAGE_URL = 'https://gwy15.com'
 API_URL = PAGE_URL + '/api'
-ROUTES = [
-    ['/', 'index'],
-    ['/blog/', 'blog/index'],
-    # ...
-]
+
 OUTPUT_PATH = Path('output')
-USER_AGENT = 'gwy15-prerenderer'
 OPTIONS = {
     'headless': True,
     'args': [
-        f'--user-agent={USER_AGENT}'
+        '--user-agent=gwy15-prerenderer'
     ]
 }
 
 
-async def extend_routes(routes):
-    url = API_URL + '/blog/post'
-    async with aiohttp.request('GET', url) as resp:
-        data = await resp.json()
-    for item in data:
-        title = item['title']
-        ROUTES.append([
-            f'/blog/{title}', f'blog/{title}'
-        ])
+@dataclass
+class PageTask:
+    path: str
+    name: str
+    page_last_modified: int
+
+    def need_update(self, force: bool) -> bool:
+        if force:
+            return True
+        path = OUTPUT_PATH / self.name
+        if not path.exists():
+            return True
+        return self.page_last_modified > path.stat().st_mtime
+
+    @property
+    def url(self):
+        return PAGE_URL + quote(self.path)
+
+    @property
+    def lastmod_str(self):
+        path = OUTPUT_PATH / self.name
+        if path.exists():
+            # 预渲染文件时间
+            file_last_mod = datetime.datetime.fromtimestamp(
+                path.stat().st_mtime)
+            # 源文件修改时间
+            source_last_mod = datetime.datetime.fromtimestamp(
+                self.page_last_modified)
+            t = max(file_last_mod, source_last_mod)
+        else:
+            # 预渲染时间为今天的时间
+            t = datetime.date.today()
+
+        return t.strftime('%Y-%m-%d')
+
+    def __repr__(self):
+        t = datetime.datetime.fromtimestamp(self.page_last_modified)
+        return f'<Task {self.name} lastmod={t.strftime("%Y-%m-%d %H:%M:%S")}>'
 
 
-async def generate_site_map(routes):
-    template = """<?xml version="1.0" encoding="UTF-8" ?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+class TaskFactory:
+    @staticmethod
+    async def generate_tasks() -> Generator[PageTask, None, None]:
+        # /
+        yield PageTask(path='/', name='index.html', page_last_modified=time())
+
+        # blogs
+        async for task in TaskFactory.generate_blog_tasks():
+            yield task
+
+    @staticmethod
+    async def generate_blog_tasks() -> Generator[PageTask, None, None]:
+        blog_api_url = API_URL + '/blog/post'
+        async with aiohttp.request('GET', blog_api_url) as resp:
+            posts = await resp.json()
+        # /blog/
+        yield PageTask(
+            path='/blog/', name='blog/index.html',
+            page_last_modified=max(post['content']['modified'] for post in posts))
+        # /blog/:title
+        for post in posts:
+            title = post['title']
+            yield PageTask(
+                path=f'/blog/{title}',
+                name=f'blog/{title}',
+                page_last_modified=post['content']['modified'])
+
+
+class Prerenderer:
+    async def run(self, force: bool):
+        tasks = []
+        async for task in TaskFactory.generate_tasks():
+            tasks.append(task)
+
+        # generate sitemap
+        await self.generate_sitemaps(tasks)
+        print('Sitemap generated.')
+
+        # prerender
+        await self.generate_prerender_pages(tasks, force)
+
+    async def save(self, content: str, name: str):
+        """Write content to file"""
+        path = OUTPUT_PATH / name
+        path.parent.mkdir(parents=True, exist_ok=True)
+        with path.open('w', encoding='utf8') as f:
+            f.write(content)
+
+    async def generate_sitemaps(self, tasks: List[PageTask]) -> None:
+        XML_TEMPLATE = """<?xml version="1.0" encoding="UTF-8" ?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         {}
-    </urlset>"""
-    url_template = """
-    <url>
-        <loc>{url}</loc>
-        <lastmod>{date}</lastmod>
-        <changefreq>daily</changefreq>
-    </url>"""
+        </urlset>"""
+        XML_URL_TEMPLATE = """
+        <url>
+            <loc>{url}</loc>
+            <lastmod>{date}</lastmod>
+            <changefreq>daily</changefreq>
+        </url>"""
 
-    def route_to_info(route):
-        return {
-            'url': PAGE_URL + quote(route[0]),
-            'date': datetime.date.today().strftime('%Y-%m-%d')
-        }
+        xml_urls = '\n'.join(
+            XML_URL_TEMPLATE.format(
+                url=task.url,
+                date=task.lastmod_str)
+            for task in tasks)
+        xml_sitemap = XML_TEMPLATE.format(xml_urls)
+        await self.save(xml_sitemap, 'sitemap.xml')
 
-    urls = '\n'.join(
-        url_template.format(**route_to_info(route))
-        for route in routes
-    )
-    result = template.format(urls)
-    await save(result, OUTPUT_PATH / 'sitemap.xml')
+        plaintext = '\n'.join(task.url for task in tasks)
+        await self.save(plaintext, 'sitemap.txt')
 
-    plaintext = '\n'.join(
-        PAGE_URL + quote(route[0])
-        for route in routes
-    )
-    await save(plaintext, OUTPUT_PATH / 'sitemap.txt')
-    print('sitemap generated.')
+    async def generate_prerender_pages(self, tasks: List[PageTask], force: bool) -> None:
+        browser = await launch(OPTIONS)
 
+        for task in tasks:
+            if not task.need_update(force):
+                continue
+            page = await browser.newPage()
+            print(f'Generating {task}...')
+            await page.goto(task.url)
+            await asyncio.sleep(0.5)
 
-async def save(content: str, path: Path):
-    path.parent.mkdir(parents=True, exist_ok=True)
-    with path.open('w', encoding='utf8') as f:
-        f.write(content)
-
-
-async def generate_prerender_pages():
-    browser = await launch(OPTIONS)
-
-    for route, name in ROUTES:
-        page = await browser.newPage()
-        await page.setUserAgent(USER_AGENT)
-        print(f'Generating {name}...')
-        await page.goto(PAGE_URL + route)
-        await asyncio.sleep(0.5)
-        content = await page.content()
-        await save(content, OUTPUT_PATH / (name + '.html'))
-        await save(content, OUTPUT_PATH / name)
-        print(f'Page {name} generated.')
-        # await page.close()
-    await asyncio.sleep(1)
-    await browser.close()
+            content = await page.content()
+            await self.save(content, task.name)
+            print(f'Page {task.name} generated.')
+            # await page.close()
+        await asyncio.sleep(1)
+        await browser.close()
 
 
-async def main():
-    # shutil.rmtree(str(OUTPUT_PATH), ignore_errors=True)
-    # OUTPUT_PATH.mkdir(exist_ok=True)
-    #
-    await extend_routes(ROUTES)
-
-    # generate sitemap.txt and xml
-    await generate_site_map(ROUTES)
-
-    #
-    await generate_prerender_pages()
+@click.command()
+@click.option('--force', is_flag=True, help='Force regenerate all pages')
+def run(force: bool):
+    asyncio.run(Prerenderer().run(force))
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    run()
