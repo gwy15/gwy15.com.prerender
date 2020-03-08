fcd61853208c12b39c1e6fc31fba8814f6a43c10
gwy15@ECS <gwy15thu@gmail.com>
2020-03-08 18:11:05 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 set -e
 cd "${0%/*}"
-# clear output directory
-rm    -rf output && mkdir -p output
+# make output directory
+mkdir -p output
 # change permissions
 chown -R 999:999    output && chmod -R 775  output
 docker run -it --rm --init --cap-add=SYS_ADMIN \
@@ -10,3 +10,4 @@ docker run -it --rm --init --cap-add=SYS_ADMIN \
     prerenderer
 # hand over permission to main user
 chown -R 1000:1000  output && chmod -R 775  output
+
